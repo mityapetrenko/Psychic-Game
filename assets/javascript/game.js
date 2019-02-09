4b2a52d5fe47532ebff04f03c3a8e46e994fe3f3
mityapetrenko <dimbaslv@gmail.com>
2019-02-08 22:51:48 -0600

    structure

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,42 @@
 
-  var alphabet="abcdefghijklmnopqrstuvwxyz";
+  var alphabet=["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var wins=0;
   var losses=0;
   var leftGuesses=10;
-  var userGuesses=[];
+  var userGuesses= [];
 
-  var cGText=document.getElementById("computer-Guess");
-  var losText=document.getElementById("losses");
-  var winText=document.getElementById("wins");
-  var leftoverGsText=document.getElementById("leftoverGuesses");
-  var userGsText=document.getElementById("userGuesses");
 
-  var userChoices=document.onkeyup = function(event) {
-
+ 
+  document.onkeyup = function(event) {
+    var userChoice=event.key;
+    var options=["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
+    if (options.indexOf(userChoice) > -1 ){
+        leftGuesses=10;
+        userChoice.push([userGuesses]);
 
-    if
+        if (userChoice===computerGuess) {
+        wins++; 
+        leftGuesses=10;
+        userChoice.push([userGuesses]);}
+
+        if (userChoice != computerGuess) {
+            leftGuesses--;
+            userChoice.push([userGuesses]);
+        }
+
+        if (leftGuesses===0) {
+        losses ++; 
+        leftGuesses=10; 
+
+        } 
+        
+        
+    document.getElementById("losses").innerHTML="Losses: " + losses;
+    document.getElementById("wins").innerHTML= "Wins: " + wins;
+    document.getElementById("leftoverGuesses").innerHTML= "Guesses left: " + leftGuesses;
+    document.getElementById("userGuesses").innerHTML= "Your guesses so far: " + userGuesses;
+    }
+    
   };
-
